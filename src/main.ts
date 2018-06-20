1e7975bbc7afda8c7e39949f64b344c78c16b810
Evgeniy <aka47Hitman@gmail.com>
2018-06-20 12:43:30 +0500

    add uiv in project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import * as uiv from 'uiv';
+
+Vue.use(uiv);
 
 Vue.config.productionTip = false;
 
